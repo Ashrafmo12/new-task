4911031cc0da74222817724b652c72cefe007a58
Ashrafmo12 <adamwaleedswewdy@gmail.com>
2024-05-26 10:57:08 +0300

    first commit

---
+++ Task/fun teach/java.js
@@ -0,0 +1,18 @@
+function sound1()
+{
+ window.open('lion-roar-6011.mp3');
+}
+function sound2()
+{
+ window.open('cat-98721.mp3');
+}
+function next()
+{
+    var d=document.getElementById("ll");
+    d.src='download.jpg';
+}
+function back()
+{
+    var d=document.getElementById("ll");
+    d.src='download (1).jpg';
+}
